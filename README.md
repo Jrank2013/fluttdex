821c8be4c9a1d4b4c9686ccd3ef97485eeba5eff
Jamario Rankins <jaamariorankins2013@gmail.com>
2021-06-23 16:04:44 -0600

    Fix flutter lint problems

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
 # fluttdex
+[![Codemagic build status](https://api.codemagic.io/apps/60d39672ab5163e7dc655b82/60d39672ab5163e7dc655b81/status_badge.svg)](https://codemagic.io/apps/60d39672ab5163e7dc655b82/60d39672ab5163e7dc655b81/latest_build)
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+A PokeDex built in flutter, part of a blog series on [avgcoder.dev](https://avgcoder.dev)
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}): super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      key: key,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -45,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'You have pushed the button this many times:',
             ),
             Text(
@@ -58,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
     );
   }
